8b3587a491eed1ad96f08ae54fc39cff8acb59e0
Vernieri <jvmendes.ti@gmail.com>
2018-10-19 16:10:25 -0300

    Create server.js

---
+++ cam-api/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var cams = require('./routes/camera-api');
+var players = require('./routes/player-api');
+var positions = require('./routes/positions-api');
+var accounts = require('./routes/account-api');
+
+var port = 3000;
+
+var app = express();
+
+// View engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//set static folder, angular
+app.use(express.static(path.join(__dirname, 'client')));
+
+//Body Parser Middleware
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', index);
+app.use('/api', cams);
+app.use('/api', players);
+app.use('/api', positions);
+app.use('/api', accounts);
+
+app.listen(port, function(){
+	console.log('[+] Server started on port '+port);
+});
