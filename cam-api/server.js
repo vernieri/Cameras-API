c741958a163c50a2223c3aa34d56001d091cfe26
Vernieri <jvmendes.ti@gmail.com>
2019-03-17 18:02:12 -0300

    Update server.js

--- cam-api/server.js
+++ cam-api/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var cams = require('./routes/camera-api');
 var players = require('./routes/player-api');
